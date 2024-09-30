26c09d21203b69a8a992e404bba0a6e073025383
MICHELLE SOFIA GARBER <48521612@ort>
2024-09-30 10:42:46 -0300

    hasta ejercicio 8

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,18 +1,69 @@
 // Ejercicio 1
+function ObtenerEdad() {
+    const Nombre = document.getElementById('Nombre').value;
+    const edad = calcularEdad();
+    resultadoEdad.innerHTML = "Hola " + Nombre + ", Tenes " + edad + " años";
+}
+
 
 //Ejercicio 2
 
 function hayFruta() {
-    const frutaIngreso = document.getElementById('fruta').value;
+    const frutaIngreso = document.getElementById('frutaInput').value;
     let frutas = ["manzana", "banana", "mandarina", "durazno", "pera", "uvas", "sandia", "melón", "frutilla", "kiwi"];
     
     let resultadoFrutas = frutas.filter(fruta => fruta === frutaIngreso.toLowerCase());
-
-    const fruta = document.getElementById('fruta');
+    const resultadoElemento = document.getElementById('resultadoFruta');
 
     if (resultadoFrutas.length > 0) { 
-        fruta.innerHTML = "¡Sí! Tenemos " + frutaIngreso;
-    } else {
-        fruta.innerHTML = "No tenemos " + frutaIngreso;
+        resultadoElemento.innerHTML = "¡Sí! Tenemos " + frutaIngreso;
+    } 
+    else {
+        resultadoElemento.innerHTML = "No tenemos " + frutaIngreso;
     }
+}
+
+//Ejercicio 4
+function YoObjeto(){
+const ciudad = {
+    Nombre: "Praga",
+    FechaFun: 950-12-12,
+    Poblacion: 1309000,
+    Extension: 496
+}
+for(const propiedad in ciudad ){
+    console.log(`${propiedad} : ${ciudad[propiedad]}`)
+}
+}
+
+//Ejercicio 5
+function dobleElementos(){
+    const numeros = [1,23,46,7,90];
+    const dobles = numeros.map(numero => numero * 2);
+    console.log("Los numeros originales: "+ numeros + ". Los dobles: " +  dobles);
+}
+//Ejercicio 6
+function TrianguloAsteriscos1(){
+    const ast = "*"
+    for(i = 1; i <= 5; i++)
+    {
+        console.log(ast.repeat(i))
+    }
+}
+function TrianguloAsteriscos2(){
+    let renglon = "----*----"
+    for(i = 0; i <= 5; i++)
+    {
+        console.log(renglon);
+        renglon = renglon.replace("-*","**");
+        renglon = renglon.replace("*-", "**");
+    }
+}
+
+//Ejercicio 7
+function NombresA(){
+    const nombres = document.getElementById('ListaNombres').value;
+    const lista  = nombres.split(',');
+    const palabrasA = lista.filter(nombre => nombre.startsWith("a"));
+    console.log(palabrasA);
 }
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,16 @@
-export function obtenerDia(){
-
+ function calcularEdad(){
+    const fechaNacimiento = document.getElementById('fechaNac').value;
+    const nacimiento  = fechaNacimiento.split('-');
+    const Hoy = new Date();
+    let edad = Hoy.getFullYear() - nacimiento[0];
+    const mes = nacimiento[1];
+    if(mes > (Hoy.getMonth + 1)){
+        edad = edad--;
+    }
+    else if( mes === (Hoy.getMonth() + 1)){
+        if(nacimiento[2] > Hoy.getDay()){
+            edad = edad--;
+        }
+    }
+    return edad;
 }
